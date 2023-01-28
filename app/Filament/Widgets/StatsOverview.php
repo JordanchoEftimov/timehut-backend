7c39b2461498c79008026c9b8182a2120fb608ae
Jordancbo Eftimov <ejordancho@gmail.com>
2023-01-29 00:11:40 +0100

    Added stats widgets

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Company;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getCards(): array
+    {
+        return [
+            Card::make('Регистрирани компании', Company::query()->count())
+                ->description('Број на регистрирани компании на Timehut')
+                ->chart([7, 2, 10, 3, 15, 4, 17]),
+            Card::make('Активни компании', Company::query()->where('active', true)->count())
+                ->description('Број на активни компании на Timehut')
+                ->chart([2, 2, 5, 10, 15, 4, 17]),
+        ];
+    }
+}

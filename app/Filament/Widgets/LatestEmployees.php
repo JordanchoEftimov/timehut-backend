ec3a7d2b77f0f3999d558844b85fed7c294664d6
JordanchoEftimov <ejordancho@gmail.com>
2023-07-31 16:43:30 +0200

    lastest employees widgeg

---
+++ app/Filament/Widgets/LatestEmployees.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Enums\UserType;
+use App\Models\Employee;
+use Closure;
+use Filament\Tables;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Builder;
+
+class LatestEmployees extends BaseWidget
+{
+    protected static ?string $heading = 'Последни регистрирани вработени';
+
+    public static function canView(): bool
+    {
+        return auth()->user()->type->value === UserType::COMPANY->value;
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        return Employee::query()->latest();
+    }
+
+    protected function getTableColumns(): array
+    {
+        return [
+            Tables\Columns\TextColumn::make('name')
+                ->label('Име'),
+            Tables\Columns\TextColumn::make('surname')
+                ->label('Презиме'),
+        ];
+    }
+}

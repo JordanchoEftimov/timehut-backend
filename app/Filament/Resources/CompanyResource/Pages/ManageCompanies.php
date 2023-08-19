48114afd40c3f4f5185b6197e825333e34116089
JordanchoEftimov <ejordancho@gmail.com>
2023-08-20 00:50:01 +0200

    Wizard form for creating company

--- app/Filament/Resources/CompanyResource/Pages/ManageCompanies.php
+++ app/Filament/Resources/CompanyResource/Pages/ManageCompanies.php
@@ -4,6 +4,10 @@ namespace App\Filament\Resources\CompanyResource\Pages;
 
 use App\Filament\Resources\CompanyResource;
 use App\Models\User;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard\Step;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ManageRecords;
 
@@ -26,7 +30,39 @@ class ManageCompanies extends ManageRecords
                     $data['user_id'] = $user->id;
 
                     return $data;
-                }),
+                })
+                ->steps([
+                    Step::make('Податоци за компанијата')
+                        ->description('Внесете ги основните податоци за компанијата')
+                        ->schema([
+                            FileUpload::make('avatar_url')
+                                ->image()
+                                ->imageEditor(),
+                            TextInput::make('name')
+                                ->required()
+                                ->label('Име'),
+                            TextInput::make('address')
+                                ->required()
+                                ->maxLength(255)
+                                ->label('Адреса'),
+                            Checkbox::make('active')
+                                ->label('Активна'),
+                        ]),
+                    Step::make('Податоци за најава')
+                        ->description('Податоци за најава на системот за компанијата')
+                        ->schema([
+                            TextInput::make('email')
+                                ->email()
+                                ->unique(table: User::class)
+                                ->required()
+                                ->label('Е-пошта'),
+                            TextInput::make('password')
+                                ->password()
+                                ->required()
+                                ->hiddenOn('edit')
+                                ->label('Лозинка'),
+                        ]),
+                ]),
         ];
     }
 }

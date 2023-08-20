131526738a9fcf63774624b493f557539ad4c939
JordanchoEftimov <ejordancho@gmail.com>
2023-08-20 16:50:48 +0200

    Login api endpoint

---
+++ src/app/Http/Controllers/V1/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\LoginRequest;
+use App\Http\Resources\V1\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    /**
+     * @throws ValidationException
+     */
+    public function login(LoginRequest $request): UserResource
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Не точна лозинка или е-пошта.'],
+            ]);
+        }
+        $token = $user->createToken($request->device_name)->plainTextToken;
+
+        return UserResource::make($user)
+            ->additional(['token' => $token]);
+    }
+}
--- src/routes/api/v1.php
+++ src/routes/api/v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\V1\AuthController;
 use App\Http\Controllers\V1\ShiftController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::prefix('/auth')->name('auth.')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {

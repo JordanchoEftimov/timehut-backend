c33e7fdca34a44fdd2b6c3e865980fb9b8b07b70
JordanchoEftimov <ejordancho@gmail.com>
2023-08-20 16:40:52 +0200

    Disable registration

--- src/app/Providers/Filament/AdminPanelProvider.php
+++ src/app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->profile()
             ->passwordReset()
-            ->registration()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([

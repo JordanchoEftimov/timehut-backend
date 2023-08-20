064b767afb169b4aab6e9dd759e0f49047458307
JordanchoEftimov <ejordancho@gmail.com>
2023-08-20 18:20:40 +0200

    additional tests for shifts

--- src/tests/Feature/V1/ShiftsTest.php
+++ src/tests/Feature/V1/ShiftsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Employee;
+use App\Models\Shift;
 use App\Models\User;
 use Laravel\Sanctum\Sanctum;
 
@@ -41,6 +42,33 @@ it('cannot get another user\'s shifts when authenticated', function () {
     $employee = Employee::query()->firstWhere('user_id', $user->id);
 
     $response = $this->getJson(route('v1.shifts.get', $employee));
+
+    $response->assertForbidden();
+});
+
+it('can start a shift if the user is an employee', function () {
+    $user = User::factory()->has(Employee::factory())->create();
+    Sanctum::actingAs($user);
+    $employee = Employee::query()->firstWhere('user_id', $user->id);
+    $response = $this->postJson(route('v1.shifts.start', $employee));
+
+    $response->assertCreated();
+    $this->assertDatabaseHas('shifts', [
+        'id' => $response->json()['data']['id'],
+    ]);
+});
+
+it('cannot end a shift if the user did not start the shift', function () {
+    $user = User::factory()->has(Employee::factory())->create();
+    $anotherUser = User::factory()->has(Employee::factory())->create();
+    Sanctum::actingAs($user);
+    $employee = Employee::query()->firstWhere('user_id', $user->id);
+    $startedShift = $this->postJson(route('v1.shifts.start', $employee));
+
+    Sanctum::actingAs($anotherUser);
+    $employee = Employee::query()->firstWhere('user_id', $anotherUser->id);
+    $shift = Shift::query()->firstWhere('id', $startedShift->json()['data']['id']);
+    $response = $this->postJson(route('v1.shifts.end', ['employee' => $employee, 'shift' => $shift]));
 
     $response->assertForbidden();
 });

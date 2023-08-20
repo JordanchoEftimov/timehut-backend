1ea54439652c2d10c4b821de7b6a4a590e6e403e
JordanchoEftimov <ejordancho@gmail.com>
2023-08-20 18:31:04 +0200

    end shift test

--- src/tests/Feature/V1/ShiftsTest.php
+++ src/tests/Feature/V1/ShiftsTest.php
@@ -72,3 +72,15 @@ it('cannot end a shift if the user did not start the shift', function () {
 
     $response->assertForbidden();
 });
+
+it('can end shift if the user started it', function () {
+    $user = User::factory()->has(Employee::factory())->create();
+    Sanctum::actingAs($user);
+    $employee = Employee::query()->firstWhere('user_id', $user->id);
+    $startedShift = $this->postJson(route('v1.shifts.start', $employee));
+
+    $shift = Shift::query()->firstWhere('id', $startedShift->json()['data']['id']);
+    $response = $this->postJson(route('v1.shifts.end', ['employee' => $employee, 'shift' => $shift]));
+
+    $response->assertOk();
+});

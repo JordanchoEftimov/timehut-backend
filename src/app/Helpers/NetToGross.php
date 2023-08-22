f91c4b57c5c0a4a4508edceea65eb2acf7f74f0c
JordanchoEftimov <ejordancho@gmail.com>
2023-08-22 14:28:24 +0200

    fixed net to gross function

--- src/app/Helpers/NetToGross.php
+++ src/app/Helpers/NetToGross.php
@@ -4,86 +4,12 @@ namespace App\Helpers;
 
 class NetToGross
 {
-    public static function netToGross($net)
+    public static function netToGross($net): float
     {
-        // Constants
-        $maxSafeInt = pow(2, 53) - 1;
-        $maxGrossNetRatio = 10;
-        $averageSalary = 43509;
-        $personalAllowance = 9038;
-        $minNet = 15229;
-        $minGross = 22146;
-        $maxBase = $averageSalary * 16;
-        $minBase = $averageSalary / 2;
+        $grossWithoutContributions = ($net - 903.8) / 0.9;
 
-        // Function to calculate taxes based on net
-        $calculateTaxes = function ($net) use ($personalAllowance) {
-            $incomeTaxRate = 0.1; // Income tax rate
+        $gross = $grossWithoutContributions / 0.72;
 
-            return ($net - $personalAllowance) * $incomeTaxRate;
-        };
-
-        // Function to calculate contributions based on gross
-        $calculateContributions = function ($gross) {
-            $pension = 0.188; // Pension contribution rate
-            $health = 0.075; // Health contribution rate
-            $unemployment = 0.012; // Unemployment contribution rate
-            $sickness = 0.005; // Sickness contribution rate
-
-            return [
-                'pension' => $gross * $pension,
-                'health' => $gross * $health,
-                'unemployment' => $gross * $unemployment,
-                'sickness' => $gross * $sickness,
-            ];
-        };
-
-        // Binary search function to find gross
-        $findGross = function ($net) use (
-            $maxSafeInt,
-            $minGross,
-            $calculateTaxes,
-            $calculateContributions,
-            $personalAllowance
-        ) {
-            $low = $minGross;
-            $high = $maxSafeInt;
-
-            while ($low <= $high) {
-                $mid = $low + floor(($high - $low) / 2);
-                $calculatedNet = self::grossToNet($mid, $personalAllowance, $calculateTaxes, $calculateContributions);
-
-                if ($net < $calculatedNet) {
-                    $high = $mid - 1;
-                } elseif ($net > $calculatedNet) {
-                    $low = $mid + 1;
-                } else {
-                    return $mid;
-                }
-            }
-
-            return $high;
-        };
-
-        // Main logic
-        $net = max($net, $minNet);
-        $gross = $findGross($net);
-
-        return $gross;
-    }
-
-    private static function grossToNet($gross, $personalAllowance, $calculateTaxes, $calculateContributions)
-    {
-        // Calculate taxes
-        $taxableBase = $gross - $calculateContributions($gross)['pension'] - $personalAllowance;
-        $taxes = $calculateTaxes($taxableBase);
-
-        // Calculate contributions
-        $contributions = array_sum($calculateContributions($gross));
-
-        // Calculate net
-        $net = $gross - $contributions - $taxes;
-
-        return $net;
+        return round($gross);
     }
 }

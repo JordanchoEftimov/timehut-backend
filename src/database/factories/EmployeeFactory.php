6fece451e29d3264f40bbe64471307313d4c5c85
JordanchoEftimov <ejordancho@gmail.com>
2023-09-17 13:11:31 +0200

    factory for employee fixed

--- src/database/factories/EmployeeFactory.php
+++ src/database/factories/EmployeeFactory.php
@@ -24,10 +24,10 @@ class EmployeeFactory extends Factory
             'phone' => fake()->phoneNumber,
             'address' => fake()->address,
             'employment_date' => fake()->dateTime,
-            'email' => fake()->email,
             'company_id' => Company::query()->inRandomOrder()->first(),
             'user_id' => User::query()->inRandomOrder()->first(),
             'net_salary' => fake()->numberBetween(20000, 30000),
+            'previous_work_months' => fake()->numberBetween(0, 96),
         ];
     }
 }
